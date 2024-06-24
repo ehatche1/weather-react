f3ba0c9936f91331b01f3743eae825f39adfb18e
ehatche1 <140868222+ehatche1@users.noreply.github.com>
2024-06-23 22:05:33 -0400

    added axios integration so that it shows the weather of a city

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Temperature from "./Temperature";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World </h1>
+        <Temperature />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Temperature.js
@@ -0,0 +1,6 @@
+import React from "react";
+import axios from "axios";
+
+export default function Temperature() {
+  return <h2>Hello from Temperature </h2>;
+}
