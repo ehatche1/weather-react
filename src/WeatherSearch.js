369e3fe66c3df0d5783c576c99d09ad081efcf5a
ehatche1 <140868222+ehatche1@users.noreply.github.com>
2024-06-24 23:20:00 -0400

    updated packages

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -58,14 +58,3 @@ export default function WeatherSearch() {
     return form;
   }
 }
-
-<footer>
-  This project was coded by{" "}
-  <a href="https://github.com/ehatche1" target="_blank">
-    Egypt Hatchett
-  </a>
-  and is{" "}
-  <a href="https://github.com/ehatche1/weather-react" target="_blank">
-    open-sourced on Github
-  </a>
-</footer>;
