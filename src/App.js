ba82c9f855e84b57b32b67d329d89b64a27e34aa
ehatche1 <140868222+ehatche1@users.noreply.github.com>
2024-06-25 15:36:55 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -10,6 +10,25 @@ function App() {
         <h1>Weather App 🌏 </h1>
         <WeatherSearch />
       </header>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/ehatche1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Egypt Hatchett{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/ehatche1/weather-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
